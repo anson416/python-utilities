1b7113dccbfadc54bae5d37ec4e4786f972152e1
Anson416 <lamyiufung2003@gmail.com>
2023-08-05 15:07:18 +0800

    Add comments

--- utils/config.py
+++ utils/config.py
@@ -10,6 +10,16 @@ from .types import Pathlike
 
 @beartype
 def load_json(json_path: Pathlike) -> Any:
+    """
+    Load a .json file.
+
+    Args:
+        json_path (Pathlike): Path to .json
+
+    Returns:
+        Any: Loaded .json
+    """
+
     assert get_basename(json_path, split_ext=True)[1].lower() == ".json", ".json required"
 
     import json
@@ -20,6 +30,16 @@ def load_json(json_path: Pathlike) -> Any:
 
 @beartype
 def load_jsonl(jsonl_path: Pathlike) -> list[Any]:
+    """
+    Load a .jsonl file.
+
+    Args:
+        jsonl_path (Pathlike): Path to .jsonl
+
+    Returns:
+        list[Any]: Loaded .json
+    """
+
     assert get_basename(jsonl_path, split_ext=True)[1].lower() == ".jsonl", ".jsonl required"
 
     import json
@@ -30,6 +50,17 @@ def load_jsonl(jsonl_path: Pathlike) -> list[Any]:
 
 @beartype
 def load_yaml(yaml_path: Pathlike, safe: bool = True) -> Any:
+    """
+    Load a .yaml file.
+
+    Args:
+        yaml_path (Pathlike): Path to .yaml
+        safe (bool, optional): If True, load `yaml_path` safely. Defaults to True.
+
+    Returns:
+        Any: Loaded .yaml
+    """
+
     assert get_basename(yaml_path, split_ext=True)[1].lower() == ".yaml", ".yaml required"
 
     try:
@@ -43,6 +74,16 @@ def load_yaml(yaml_path: Pathlike, safe: bool = True) -> Any:
 
 @beartype
 def load_ini(ini_path: Pathlike) -> Any:
+    """
+    Load a .ini file.
+
+    Args:
+        ini_path (Pathlike): Path to .ini
+
+    Returns:
+        ConfigParser: Loaded .ini
+    """
+
     @beartype
     def _load_ini(ini_path: Pathlike) -> ConfigParser:
         config = ConfigParser()
@@ -59,6 +100,16 @@ def load_ini(ini_path: Pathlike) -> Any:
 
 @beartype
 def load_xml(xml_path: Pathlike) -> Any:
+    """
+    Load a .xml file.
+
+    Args:
+        xml_path (Pathlike): Path to .xml
+
+    Returns:
+        BeautifulSoup: Loaded .xml
+    """
+
     @beartype
     def _load_xml(xml_path: Pathlike) -> BeautifulSoup:
         with open(xml_path, "r") as f_xml:
