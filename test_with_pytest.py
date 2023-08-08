fbaf890949dae6ef0edc36cb6fb763d0b9aa7dc0
Anson416 <lamyiufung2003@gmail.com>
2023-08-08 14:42:09 +0800

    Create test_with_pytest.py

---
+++ test_with_pytest.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+# File: test_with_pytest.py
+
+from utils.color import *
+from utils.config import *
+from utils.date_time import *
+from utils.downloader import *
+from utils.error import *
+from utils.file_ops import *
+from utils.formatter import *
+from utils.hardware import *
+from utils.logger import *
+from utils.num_ops import *
+from utils.types import *
+from utils.utils import *
